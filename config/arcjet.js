af516a8a29258efe03ea2a03a3dc945053003337
Saif Abdelrazek <saif@saifabdelrazek.com>
2025-06-10 20:46:27 +0300

    Make arcjet more strict in time limiting and allowed more bots

--- config/arcjet.js
+++ config/arcjet.js
@@ -2,22 +2,28 @@ import arcjet, { shield, detectBot, tokenBucket } from "@arcjet/node";
 import { ARCJET_KEY, ARCJET_ENV } from "./env.js";
 
 const aj = arcjet({
-    key: ARCJET_KEY,
-    characteristics: ["ip.src"],
-    rules: [
-        shield({ mode: "LIVE" }),
+  key: ARCJET_KEY,
+  characteristics: ["ip.src"],
+  rules: [
+    shield({ mode: "LIVE" }),
 
-        detectBot({
-            mode: "LIVE",
-            allow: ["CATEGORY:SEARCH_ENGINE", ARCJET_ENV === "development" ? "POSTMAN" : "",],
-        }),
-        tokenBucket({
-            mode: "LIVE",
-            refillRate: 5, // Refill 5 tokens per interval
-            interval: 10, // Refill every 10 seconds
-            capacity: 10, // Bucket capacity of 10 tokens
-        }),
-    ],
+    detectBot({
+      mode: "LIVE",
+      allow: [
+        "CATEGORY:SEARCH_ENGINE",
+        "POSTMAN",
+        "CURL",
+        "HTTP_CLIENT",
+        "API_CLIENT",
+      ],
+    }),
+    tokenBucket({
+      mode: "LIVE",
+      refillRate: 5, // Refill 5 tokens per interval
+      interval: ARCJET_ENV === "development" ? 10 : 60, // Refill every 10 seconds
+      capacity: 10, // Bucket capacity of 10 tokens
+    }),
+  ],
 });
 
 export default aj;
